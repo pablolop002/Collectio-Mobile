cee6aa82d56e190a1e2fc0d6a734dab36ddb3b31
pablolop002 <pablolop002@me.com>
2021-02-14 12:23:30 +0100

    M-V-VM pattern on ItemsView

--- Collectio/ViewModels/ItemsViewModel.cs
+++ Collectio/ViewModels/ItemsViewModel.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Collectio.Models;
+using Collectio.Resources.Culture;
+using Collectio.Utils;
 using MvvmHelpers;
 using MvvmHelpers.Commands;
 
@@ -8,21 +12,45 @@ namespace Collectio.ViewModels
     {
         private bool _isRefreshing;
         private Collection _collection;
+        private Item _selectedItem;
 
         public ObservableRangeCollection<Item> Items { get; private set; }
-        
-        public Command RefreshCommand { get; set; }
+
+        public ICommand RefreshCommand { get; }
+
+        public ICommand AddCommand { get; }
+
+        public ICommand DeleteCommand { get; }
+
+        public ICommand DuplicateCommand { get; }
+
+        public ICommand EditCommand { get; }
 
         public Collection Collection
         {
             get => _collection;
             set
             {
+                SetProperty(ref _collection, value);
+                if (value == null) return;
+                Title = value.Name;
+                IsRefreshing = true;
+            }
+        }
+
+        public Item SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
                 if (value != null)
                 {
-                    Title = value.Name;
+                    Xamarin.Forms.Shell.Current.GoToAsync($"item?item={value.Id.ToString()}");
+                    value = null;
                 }
-                SetProperty(ref _collection, value);
+
+                _selectedItem = value;
+                OnPropertyChanged();
             }
         }
 
@@ -39,16 +67,11 @@ namespace Collectio.ViewModels
         public ItemsViewModel()
         {
             Items = new ObservableRangeCollection<Item>();
+            AddCommand = new AsyncCommand(Add);
+            DeleteCommand = new AsyncCommand<Item>(Delete);
+            DuplicateCommand = new Command<Item>(Duplicate);
+            EditCommand = new Command<Item>(Edit);
             RefreshCommand = new Command(RefreshEvent);
-            IsRefreshing = true;
-        }
-
-        public ItemsViewModel(Collection collection)
-        {
-            Collection = collection;
-            Items = new ObservableRangeCollection<Item>();
-            RefreshCommand = new Command(RefreshEvent);
-            IsRefreshing = true;
         }
 
         private void RefreshEvent()
@@ -59,6 +82,51 @@ namespace Collectio.ViewModels
             Items.AddRange(App.DataRepo.GetAllItemsFromCategory(Collection.Id.ToString(), true));
 
             IsRefreshing = false;
+        }
+
+        private async Task Add()
+        {
+            var answer = await Xamarin.Forms.Shell.Current.DisplayActionSheet(Strings.NewItem, Strings.Cancel, null,
+                Strings.Create, Strings.Import, Strings.Duplicate);
+
+            if (answer == null || answer == Strings.Cancel) return;
+
+            if (answer == Strings.Create)
+            {
+                await Xamarin.Forms.Shell.Current.GoToAsync($"newItem?collection={Collection.Id.ToString()}");
+            }
+            else if (answer == Strings.Import)
+            {
+                await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Import, "Próximamente", Strings.Ok);
+                //await Shell.Current.GoToAsync($"importItem?collection={Collection.Id.ToString()}");
+            }
+            else
+            {
+                await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Duplicate, Strings.DuplicateMessage, Strings.Ok);
+            }
+        }
+
+        private static void Edit(Item item)
+        {
+            Xamarin.Forms.Shell.Current.GoToAsync($"editItem?item={item.Id.ToString()}");
+        }
+
+        private static void Duplicate(Item item)
+        {
+            Xamarin.Forms.Shell.Current.GoToAsync(
+                $"newItem?collection={item.CollectionId.ToString()}&copyFrom={item.Id.ToString()}");
+        }
+
+        private async Task Delete(Item item)
+        {
+            var aux = await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.SureQuestion, Strings.DeleteItem,
+                Strings.Confirm, Strings.Cancel);
+            if (!aux) return;
+
+            App.DataRepo.RemoveItem(item.Id.ToString());
+            FileSystemUtils.DeleteItem(item.CollectionId.ToString(), item.Id.ToString());
+
+            IsRefreshing = true;
         }
     }
 }
--- Collectio/Views/ItemsView.xaml.cs
+++ Collectio/Views/ItemsView.xaml.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Linq;
-using Collectio.Models;
-using Collectio.Resources.Culture;
-using Collectio.Utils;
 using Collectio.ViewModels;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,103 +10,20 @@ namespace Collectio.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemsView : ContentPage
     {
-        private string _collectionId;
-
         public string Collection
         {
-            set
-            {
-                _collectionId = value;
-                //((ItemsViewModel) BindingContext).Collection = App.DataRepo.GetCollection(Uri.UnescapeDataString(value));
-                BindingContext = new ItemsViewModel(App.DataRepo.GetCollection(Uri.UnescapeDataString(value)));
-            }
+            set => ((ItemsViewModel) BindingContext).Collection =
+                App.DataRepo.GetCollection(Uri.UnescapeDataString(value));
         }
 
         public string Refresh
         {
-            set => MainThread.BeginInvokeOnMainThread(() => RefreshItemsView.IsRefreshing = value.Equals("true"));
+            set => ((ItemsViewModel) BindingContext).IsRefreshing = value.Equals("true");
         }
 
         public ItemsView()
         {
             InitializeComponent();
-            Shell.SetTabBarIsVisible(this, false);
-
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                if (DeviceInfo.Idiom != DeviceIdiom.Tablet) return;
-                ItemsCollectionView.ItemsLayout = new GridItemsLayout(2, ItemsLayoutOrientation.Vertical)
-                {
-                    HorizontalItemSpacing = 10, VerticalItemSpacing = 10
-                };
-            });
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                ItemsCollectionView.SelectedItems = null;
-                ItemsCollectionView.SelectedItem = null;
-            });
-        }
-
-        private void ItemsView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.CurrentSelection?.FirstOrDefault() is Item item)
-            {
-                Shell.Current.GoToAsync($"item?item={item.Id.ToString()}");
-            }
-        }
-
-        private async void Add_OnClicked(object sender, EventArgs e)
-        {
-            var answer = await Shell.Current.DisplayActionSheet(Strings.NewItem, Strings.Cancel, null,
-                Strings.Create, Strings.Import, Strings.Duplicate);
-
-            if (answer == null || answer == Strings.Cancel) return;
-
-            if (answer == Strings.Create)
-            {
-                await Shell.Current.GoToAsync($"newItem?collection={_collectionId}");
-            }
-            else if (answer == Strings.Import)
-            {
-                await Shell.Current.DisplayAlert(Strings.Import, "Próximamente", Strings.Ok);
-                //await Shell.Current.GoToAsync($"importItem?collection={_collectionId}");
-            }
-            else
-            {
-                await Shell.Current.DisplayAlert(Strings.Duplicate, Strings.DuplicateMessage, Strings.Ok);
-            }
-        }
-
-        private void Edit_Invoked(object sender, EventArgs eventArgs)
-        {
-            if (!(((SwipeItemView) sender).BindingContext is Item item)) return;
-
-            Shell.Current.GoToAsync($"editItem?item={item.Id.ToString()}");
-        }
-
-        private void Duplicate_Invoked(object sender, EventArgs eventArgs)
-        {
-            if (!(((SwipeItemView) sender).BindingContext is Item item)) return;
-
-            Shell.Current.GoToAsync($"newItem?collection={item.CollectionId.ToString()}&copyFrom={item.Id.ToString()}");
-        }
-
-        private async void Delete_Invoked(object sender, EventArgs eventArgs)
-        {
-            if (!(((SwipeItemView) sender).BindingContext is Item item)) return;
-            var aux = await Shell.Current.DisplayAlert(Strings.SureQuestion, Strings.DeleteItem,
-                Strings.Confirm, Strings.Cancel);
-            if (!aux) return;
-
-            App.DataRepo.RemoveItem(item.Id.ToString());
-            FileSystemUtils.DeleteItem(item.CollectionId.ToString(), item.Id.ToString());
-
-            MainThread.BeginInvokeOnMainThread(() => RefreshItemsView.IsRefreshing = true);
         }
     }
 }
