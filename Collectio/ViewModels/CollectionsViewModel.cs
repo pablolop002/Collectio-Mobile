9a76cc434aea12d799c1bd5d02de07b896a2bc1e
pablolop002 <pablolop002@me.com>
2021-08-09 18:01:03 +0200

    Collections - Delete sync command, Collections default empty and only allow creation

--- Collectio/ViewModels/CollectionsViewModel.cs
+++ Collectio/ViewModels/CollectionsViewModel.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Collectio.Models;
@@ -37,8 +36,6 @@ namespace Collectio.ViewModels
 
         public ICommand RefreshCommand { get; set; }
 
-        public ICommand SyncCommand { get; set; }
-
         public ICommand AddCommand { get; set; }
 
         public ICommand EditCommand { get; set; }
@@ -47,9 +44,8 @@ namespace Collectio.ViewModels
 
         public CollectionsViewModel()
         {
-            CollectionGroups = new ObservableRangeCollection<CollectionGroup>(App.DataRepo.GetCollectionGroups());
+            CollectionGroups = new ObservableRangeCollection<CollectionGroup>();
             RefreshCommand = new Command(RefreshEvent);
-            SyncCommand = new AsyncCommand(Sync);
             AddCommand = new AsyncCommand(Add);
             EditCommand = new Command<Collection>(Edit);
             DeleteCommand = new AsyncCommand<Collection>(Delete);
@@ -57,15 +53,15 @@ namespace Collectio.ViewModels
 
         private static async Task Add()
         {
-            var answer = await Xamarin.Forms.Shell.Current.DisplayActionSheet(Strings.NewCollection, Strings.Cancel,
+            /*var answer = await Xamarin.Forms.Shell.Current.DisplayActionSheet(Strings.NewCollection, Strings.Cancel,
                 null, Strings.Create, Strings.Import);
 
             if (answer == null || answer == Strings.Cancel) return;
 
             if (answer == Strings.Create)
-            {
-                await Xamarin.Forms.Shell.Current.GoToAsync("newCollection");
-            }
+            {*/
+            await Xamarin.Forms.Shell.Current.GoToAsync("newCollection");
+            /*}
             else if (answer == Strings.Import)
             {
                 await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Import, "Próximamente", Strings.Ok);
@@ -96,83 +92,7 @@ namespace Collectio.ViewModels
                 var collection = csvReader.GetRecord<Collection>();
 
                 //await Xamarin.Forms.Shell.Current.GoToAsync("importCollection");
-            }
-        }
-
-        private async Task Sync()
-        {
-            var sure = await Xamarin.Forms.Shell.Current.DisplayAlert("Strings.Sure", "Strings.SyncOfflineChanges",
-                Strings.Ok, Strings.Cancel);
-
-            if (!sure) return;
-
-            var offlineActions = App.DataRepo.GetOfflineActions();
-            foreach (var offlineAction in offlineActions)
-            {
-                switch (offlineAction.Type)
-                {
-                    case "delete":
-                        switch (offlineAction.ElementType)
-                        {
-                            case "collection":
-                                await App.DataRepo.RemoveCollection(offlineAction.ElementIdentifier, offlineAction.Id);
-                                break;
-                            case "item":
-                                await App.DataRepo.RemoveItem(offlineAction.ElementIdentifier, offlineAction.Id);
-                                break;
-                            case "itemImage":
-                                await App.DataRepo.RemoveItemImage(offlineAction.ElementIdentifier, offlineAction.Id);
-                                break;
-                            default:
-                                await App.DataRepo.RemoveApikey(offlineAction.ElementIdentifier, offlineAction.Id);
-                                break;
-                        }
-
-                        break;
-                    case "update":
-                        switch (offlineAction.ElementType)
-                        {
-                            case "collection":
-                                App.DataRepo.UpdateCollection(
-                                    App.DataRepo.GetCollection(offlineAction.ElementIdentifier), offlineAction.Id);
-                                break;
-                            case "item":
-                                App.DataRepo.UpdateItem(App.DataRepo.GetItem(offlineAction.ElementIdentifier),
-                                    offlineAction.Id);
-                                break;
-                            case "itemImage":
-                                App.DataRepo.UpdateItemImage(App.DataRepo.GetItemImage(offlineAction.ElementIdentifier),
-                                    offlineAction.Id);
-                                break;
-                            case "user":
-                                await App.DataRepo.UpdateUser(await App.DataRepo.GetUser(offlineAction.Id, false),
-                                    offlineAction.Id);
-                                break;
-                            default:
-                                await App.DataRepo.UpdateApikey(App.DataRepo.GetApikey(offlineAction.ElementIdentifier),
-                                    offlineAction.Id);
-                                break;
-                        }
-
-                        break;
-                    default:
-                        switch (offlineAction.ElementType)
-                        {
-                            case "collection":
-                                App.DataRepo.AddCollectionServer(
-                                    App.DataRepo.GetCollection(offlineAction.ElementIdentifier), offlineAction.Id);
-                                break;
-                            case "item":
-                                //App.DataRepo.AddItemServer(App.DataRepo.GetItem(offlineAction.ElementIdentifier), offlineAction.Id);
-                                break;
-                            case "itemImage":
-                                //App.DataRepo.AddItemImageServer(App.DataRepo.GetItemImage(offlineAction.ElementIdentifier), offlineAction.Id);
-                                break;
-                        }
-
-                        break;
-                }
-            }
+            }*/
         }
 
         private static void Edit(Collection collection)
