ce6f89a9ae9ff8fd59415d30100eb8c04f288042
pablolop002 <pablolop002@me.com>
2021-08-09 17:18:39 +0200

    Models/User - Style code and get image from server

--- Collectio/Models/User.cs
+++ Collectio/Models/User.cs
@@ -9,23 +9,40 @@ namespace Collectio.Models
     public class User
     {
         [PrimaryKey, AutoIncrement] public int Id { get; set; }
-        
+
         [Unique] public int? ServerId { get; set; }
 
         public string Nickname { get; set; }
-        
+
         public string Mail { get; set; }
 
         public string Image { get; set; }
-        
-        public bool Apple { get; set; }
-        
-        public bool Google { get; set; }
-        
-        public bool Microsoft { get; set; }
-        
-        [OneToMany(CascadeOperations = CascadeOperation.All)] public ObservableRangeCollection<Apikey> ApiKeys { get; set; }
 
-        [Ignore, JsonIgnore] public string File => FileSystemUtils.GetProfileImage(Image);
+        public bool Apple { get; set; }
+
+        public bool Google { get; set; }
+
+        public bool Microsoft { get; set; }
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public ObservableRangeCollection<Apikey> ApiKeys { get; set; }
+
+        [Ignore, JsonIgnore]
+        public string File
+        {
+            get
+            {
+                var file = FileSystemUtils.GetProfileImage(Image ?? "default_image.png");
+                switch (file)
+                {
+                    case null:
+                        return string.Format(RestServiceUtils.RestUrl, "/images/default_image.png");
+                    case "":
+                        return string.Format(RestServiceUtils.RestUrl, $"/images/user{ServerId.ToString()}/{Image}");
+                    default:
+                        return file;
+                }
+            }
+        }
     }
 }
