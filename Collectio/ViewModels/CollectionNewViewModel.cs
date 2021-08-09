0f6fd2b0835aed35ae740e6d8d9b8a7db85a3e33
pablolop002 <pablolop002@me.com>
2021-08-09 18:01:44 +0200

    New Collection - New save

--- Collectio/ViewModels/CollectionNewViewModel.cs
+++ Collectio/ViewModels/CollectionNewViewModel.cs
@@ -1,45 +1,27 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Collectio.Models;
 using Collectio.Resources.Culture;
 using Collectio.Utils;
+using Collectio.ViewModels.BaseViewModels;
 using Microsoft.AppCenter.Analytics;
-using MvvmHelpers;
 using MvvmHelpers.Commands;
 using Plugin.StoreReview;
 using Xamarin.Essentials;
 
 namespace Collectio.ViewModels
 {
-    public class CollectionNewViewModel : BaseViewModel
+    public class CollectionNewViewModel : BaseCollectionViewModel
     {
-        private IEnumerable<Category> _categories;
         private Category _selectedCategory;
-        private Collection _collection;
         private bool _categorySelection = true;
         private bool _collectionDetails;
-        private string _imageName = string.Empty;
-        private string _file = string.Empty;
-
-        public IEnumerable<Category> Categories
-        {
-            get => _categories;
-            set => SetProperty(ref _categories, value);
-        }
 
         private static int NewCollectionUsage
         {
             get => Preferences.Get(nameof(NewCollectionUsage), 0);
             set => Preferences.Set(nameof(NewCollectionUsage), value);
-        }
-
-        public Collection Collection
-        {
-            get => _collection;
-            set => SetProperty(ref _collection, value);
         }
 
         public bool CollectionDetails
@@ -68,86 +50,16 @@ namespace Collectio.ViewModels
             set
             {
                 SetProperty(ref _selectedCategory, value);
-                _collection.CategoryId = value.Id;
+                Collection.CategoryId = value.Id;
                 CategorySelection = false;
             }
         }
 
-        public string File
-        {
-            get => _file;
-            set => SetProperty(ref _file, value);
-        }
-
         public ICommand SaveItemCommand { get; }
-
-        public ICommand SelectImageCommand { get; }
 
         public CollectionNewViewModel()
         {
             SaveItemCommand = new AsyncCommand(SaveItem);
-            SelectImageCommand = new AsyncCommand(SelectImage);
-            Collection = new Collection();
-            Categories = App.DataRepo.GetCategories();
-        }
-
-        private async Task SelectImage()
-        {
-            var selection = await Xamarin.Forms.Shell.Current.DisplayActionSheet(Strings.ImageOrigin, Strings.Cancel,
-                null,
-                Strings.Camera, Strings.Gallery);
-
-            if (selection == null || selection == Strings.Cancel) return;
-            if (selection == Strings.Camera)
-            {
-                MainThread.BeginInvokeOnMainThread(async () =>
-                {
-                    try
-                    {
-                        var photo = await MediaPicker.CapturePhotoAsync();
-                        if (photo == null) return;
-
-                        using (var stream = await photo.OpenReadAsync())
-                        {
-                            using (var memStream = new MemoryStream())
-                            {
-                                await stream.CopyToAsync(memStream);
-                                File = FileSystemUtils.TempSave(memStream, photo.FileName);
-                                _imageName = photo.FileName;
-                            }
-                        }
-                    }
-                    catch (PermissionException ex)
-                    {
-                        await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Error, ex.Message, Strings.Ok);
-                    }
-                });
-            }
-            else if (selection == Strings.Gallery)
-            {
-                MainThread.BeginInvokeOnMainThread(async () =>
-                {
-                    try
-                    {
-                        var photo = await MediaPicker.PickPhotoAsync();
-                        if (photo == null) return;
-
-                        using (var stream = await photo.OpenReadAsync())
-                        {
-                            using (var memStream = new MemoryStream())
-                            {
-                                await stream.CopyToAsync(memStream);
-                                File = FileSystemUtils.TempSave(memStream, photo.FileName);
-                                _imageName = photo.FileName;
-                            }
-                        }
-                    }
-                    catch (PermissionException ex)
-                    {
-                        await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Error, ex.Message, Strings.Ok);
-                    }
-                });
-            }
         }
 
         private async Task SaveItem()
@@ -165,25 +77,33 @@ namespace Collectio.ViewModels
                 return;
             }
 
-            _collection.CreatedAt = DateTime.Now;
-            _collection.UpdatedAt = DateTime.Now;
+            Collection.CreatedAt = DateTime.Now;
+            Collection.UpdatedAt = DateTime.Now;
 
-            if (!string.IsNullOrWhiteSpace(_imageName))
+            if (!string.IsNullOrWhiteSpace(ImageName))
             {
-                _collection.Image = _imageName;
+                Collection.Image = ImageName;
             }
 
-            App.DataRepo.AddCollection(ref _collection);
+            var id = await App.DataRepo.AddCollection(Collection);
 
-            if (!string.IsNullOrWhiteSpace(_imageName))
+            if (id != -1)
             {
-                FileSystemUtils.SaveFileFromPath(_file, _imageName, _collection.Id);
-                FileSystemUtils.ClearTempPath();
+                if (!string.IsNullOrWhiteSpace(ImageName))
+                {
+                    FileSystemUtils.SaveFileFromPath(File, ImageName, id);
+                    FileSystemUtils.ClearTempPath();
+                }
+
+                if (NewCollectionUsage++ == 5) await CrossStoreReview.Current.RequestReview(false);
+
+                await Xamarin.Forms.Shell.Current.GoToAsync("..?refresh=true");
+            }
+            else
+            {
+                await Xamarin.Forms.Shell.Current.DisplayAlert(Strings.Error, "Strings.NewCollectionError", Strings.Ok);
             }
 
-            if (NewCollectionUsage++ == 5) await CrossStoreReview.Current.RequestReview(false);
-
-            await Xamarin.Forms.Shell.Current.GoToAsync("..?refresh=true");
             Analytics.TrackEvent("CreateCollection");
         }
     }
